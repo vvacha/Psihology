5a225957fa6ab57b7284433a80439cf76872c798
vvacha <tigran_gabrielyan@mail.ru>
2018-11-03 01:14:41 +0300

    Modified templates to display posts from database.

--- question/views.py
+++ question/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+
 
 # Create your views here.
 def question_list(request):
-    return render(request, 'question/question_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    print (Post, '11111111111111')
+    return render(request, 'question/question_list.html', {'posts': posts})
+
