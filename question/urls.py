46e479d0e07f33b4633355fbbcf8a838620f5a40
vvacha <tigran_gabrielyan@mail.ru>
2018-11-03 05:29:19 +0300

    added question.

--- question/urls.py
+++ question/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.question_list, name='question_list'),
+    url(r'^question/(?P<pk>\d+)/$', views.question_detail, name='question_detail'),
 ]
--- question/views.py
+++ question/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
-
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def question_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     print (Post, '11111111111111')
     return render(request, 'question/question_list.html', {'posts': posts})
+
+
+def question_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'question/question_detail.html', {'post': post})
 
